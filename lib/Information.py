d307cee76f7c27c301c70e1d18f75d03913802b2
amirinvalid-a11y <amirinvalid@gmail.com>
2026-05-30 14:26:34 +0330

    Update Information.py

--- lib/Information.py
+++ lib/Information.py
@@ -1,10 +1,10 @@
 from .library import *
 
-admin_user_id = 123456789 #<--- آیدی ادمین
-api_id = 123456789 #<--- آی پی آی آیدی
-api_hash = 'abcdefghigklmnopqrstuvwxyz' #<--- ای پی آی هش
-helper_username = 'BotUserName' #<--- یوزر ربات هلپر بدون @
-bot_token = 'abcdefghigklmnopqrstuvwxyz:466546' #<--- توکن ربات هلپر
+admin_user_id = 6929800007 #<--- آیدی ادمین
+api_id = 37086606 #<--- آی پی آی آیدی
+api_hash = '2ea979a1e3d9160534c25230fe84bc1e' #<--- ای پی آی هش
+helper_username = 'Self577_bot' #<--- یوزر ربات هلپر بدون @
+bot_token = '8957635856:AAGJeGS__mrz6PVEPBiJhsIo4eWHZXqu9uo' #<--- توکن ربات هلپر
 
 client_id = '01e7dc6b41c3471b94efe87abeb05919'
 client_secret = '4f5f93af1ced4b0d9ba8440606803639'
